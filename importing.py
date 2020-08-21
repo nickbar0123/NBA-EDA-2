a933be93901154f25b4ff0a5960f8208eed17e0c
Nick <hdoan02@tufts.edu>
2020-08-22 02:10:16 +0700

    Testing

--- importing.py
+++
@@ -1,9 +0,0 @@
-def get_data(path):
-    import pandas as pd 
-    df = pd.read_csv(path)
-    def graphs(df):
-        first_season = df[(df['fame'] == 1) & (df['YEAR'] == df['year_start'])]['PTS'].mean()
-        second_season = df[(df['fame'] == 1) & (df['YEAR'] == df['year_start'] + 1)]['PTS'].mean()
-        third_season = df[(df['fame'] == 1) & (df['YEAR'] == df['year_start'] + 2)]['PTS'].mean()
-        fourth_season = df[(df['fame'] == 1) & (df['YEAR'] == df['year_start'] + 3)]['PTS'].mean()
-        return [first_season, second_season, third_season, fourth_season]
